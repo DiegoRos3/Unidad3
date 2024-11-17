ecf65da7a8329408f4331bc061a893274e42c691
Diego Rose <124701925+DiegoRos3@users.noreply.github.com>
2024-11-17 17:56:16 -0600

    FormJs

--- js/p2.js
+++ js/p2.js
@@ -1,16 +1,16 @@
 let nombre = prompt("Ingrese su nombre");
 let edad = prompt("Ingrese su edad");
 let pais = prompt("Ingrese su pais de residencia");
-mostrarFormulario(nombre, edad, pais);
-
-
+alert("Hola " + nombre + ", tus datos están siendo procesados...")
+let confirmacion = prompt("¿Sus datos son correctos?")
+if(confirmacion === "si"){
+    mostrarFormulario(nombre, edad, pais);
+}else{
+    alert("Registros borrados");
+}
 
 function mostrarFormulario(nombre, edad, pais){
-    alert("Hola!, tus datos son:" + "\nNombre: " + nombre + "\nEdad: " + edad + "\nPais: " + pais);
-    let confirmacion = prompt("¿Sus datos son correctos?")
-    if(confirmacion === "si"){
-        alert("Registro guardado correctamente");
-    }else{
-        alert("Registros borrados");
-    }
+    alert("Registro guardado correctamente:" + "\nNombre: " + nombre + "\nEdad: " + edad + "\nPais: " + pais);
+    
+    
 }
