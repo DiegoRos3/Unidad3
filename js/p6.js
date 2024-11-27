204b5460f0b3ec66445ccecf906a17edc5a7c8e4
Diego Rose <124701925+DiegoRos3@users.noreply.github.com>
2024-11-27 10:59:17 -0600

    p6f

--- js/p6.js
+++ js/p6.js
@@ -1,12 +1,26 @@
 function cambiarBento() {
-    const elementosBGrid = document.querySelectorAll(".grid");
+    let elementosBGrid = document.querySelectorAll(".grid");
     elementosBGrid.forEach(elemento => {
         elemento.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
         elemento.style.padding = "20px";
         elemento.style.color = "#ffff64";
         elemento.style.fontSize = "0.8rem";
         elemento.style.fontFamily = "arial";
-        elemento.style.cursor = "pointer";
         elemento.style.borderRadius = "100px";     
     });
+
+    let elementosParrafo = document.querySelectorAll("p");
+    elementosParrafo.forEach(eleP => {
+        eleP.style.color = "#" + Math.floor(Math.random()*16777215).toString(16);
+    });
+
+    let elementosH3 = document.querySelectorAll("h3");
+    elementosH3.forEach(eleH3 => {
+        eleH3.innerText = "Practica 666"
+        eleH3.style.color = "#af0c00";
+    });
+    
+    let bento = document.querySelector(".bento");
+        bento.style.gridTemplateColumns = 'repeat(1, 1fr)';
+    
 }
