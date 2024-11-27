672385adb2e6926a2b429a882eabf985433a78a0
Diego Rose <124701925+DiegoRos3@users.noreply.github.com>
2024-11-26 20:58:12 -0600

    p6

--- js/p6.js
+++ js/p6.js
@@ -1,3 +1,12 @@
-function cambiarBento(){
-    alert("Si jala")
+function cambiarBento() {
+    const elementosBGrid = document.querySelectorAll(".grid");
+    elementosBGrid.forEach(elemento => {
+        elemento.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+        elemento.style.padding = "20px";
+        elemento.style.color = "#ffff64";
+        elemento.style.fontSize = "0.8rem";
+        elemento.style.fontFamily = "arial";
+        elemento.style.cursor = "pointer";
+        elemento.style.borderRadius = "100px";     
+    });
 }
