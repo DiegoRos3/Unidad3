6fad6b485530a5cb64a45995c76f17cba1af257b
Diego Rose <124701925+DiegoRos3@users.noreply.github.com>
2024-12-01 23:24:11 -0600

    p7.2

--- js/p7.js
+++ js/p7.js
@@ -3,3 +3,10 @@ function borrarElemento(){
     documento.remove();
 }
 
+function agregarElemento(){
+    let peli = prompt("Nombre de la pelicula: ");
+    let link = prompt("Link de la pelicula: ");
+    let lista = document.createElement("li");
+    lista.innerHTML = "<a href='"+link+"'>"+peli+"</a>";
+    menu_nav.appendChild(lista);
+}
